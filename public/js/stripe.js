6809e4284d4f0c4b9f460117c5fac97f32874430
Lila Liu <lilayliu@gmail.com>
2019-09-02 01:15:33 +0100

    updated stripe method

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -2,17 +2,15 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
-export const bookRetreat = async retreatId => {
 const stripe = Stripe('pk_test_V0rfzYVORTmkHIWIwq1Eger800b1i98xvO');
 
+export const bookRetreat = async retreatId => {
   try {
     // 1) Get checkout session from API
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${retreatId}`
-    );
-    console.log(session);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    // console.log(session);
 
-    // 2) Create checkout form + charge credit card
+    // 2) Create checkout form + chanre credit card
     await stripe.redirectToCheckout({
       sessionId: session.data.session.id
     });
